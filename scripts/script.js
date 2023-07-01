5cd6559ea18b3cf9f919e64860121a17a8d372a9
C <121403479+Chxdii@users.noreply.github.com>
2023-07-01 15:03:08 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/tw.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -23,7 +23,7 @@ new Vue({
             artist: "x",
             cover: "./img/fj.png",
             source: "./mp3/starby.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -31,7 +31,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/sready.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -39,7 +39,7 @@ new Vue({
             artist: "x",
             cover: "./img/fj.png",
             source: "./mp3/noname.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -47,7 +47,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/hmm.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -63,7 +63,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/gdd.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -71,7 +71,7 @@ new Vue({
             artist: "x",
             cover: "./img/fj.png",
             source: "./mp3/flashin.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -79,7 +79,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/Fever.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -87,7 +87,7 @@ new Vue({
             artist: "x",
             cover: "./img/fj.png",
             source: "./mp3/eman.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -95,7 +95,7 @@ new Vue({
             artist: "x",
             cover: "./img/SB.png",
             source: "./mp3/Boogie.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           {
@@ -103,7 +103,7 @@ new Vue({
             artist: "x",
             cover: "./img/fj.png",
             source: "./mp3/Afroblue.mp3",
-            url: "",
+            url: "3zixty.com",
             favorited: false
           },
           
